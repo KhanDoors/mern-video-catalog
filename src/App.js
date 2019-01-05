8648699d9b5dcea09076163ac32ca73697a9f26e
unknown <syyadkhan@yahoo.com>
2019-01-04 20:57:59 -0500

    front end complete

--- src/App.js
+++ src/App.js
@@ -5,13 +5,41 @@ import "./App.css";
 import VideoList from "./components/videoList";
 import EditVideo from "./components/editVideo";
 import CreateVideo from "./components/createVideo";
+import logo from "./video-logo.jpg";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="container">
-          <h1>Video Catalog App</h1>
+          <nav className="navbar navbar-expand-lg text-white-50 bg-dark">
+            <a
+              className="navbar-brand"
+              href="https://sk-portfolio.netlify.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={logo} width="30" height="30" alt="Khandoors" />
+            </a>
+            <Link to="/" className="navabr-brand">
+              KhanDoor Video Catalog
+            </Link>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">
+                    Videos
+                  </Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">
+                    Create Video Lesson
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
           <Route path="/" exact component={VideoList} />
           <Route path="/edit/:id" component={EditVideo} />
           <Route path="/create" component={CreateVideo} />
--- src/components/createVideo.js
+++ src/components/createVideo.js
@@ -1,10 +1,113 @@
 import React, { Component } from "react";
 
 export class createVideo extends Component {
+  state = {
+    video_description: "",
+    video_responsible: "",
+    video_priority: "",
+    video_completed: false
+  };
+
+  onChangeVideoDescription = e => {
+    this.setState({
+      video_description: e.target.value
+    });
+  };
+
+  onChangeVideoResponsible = e => {
+    this.setState({
+      video_responsible: e.target.value
+    });
+  };
+
+  onChangeVideoPriority = e => {
+    this.setState({
+      video_priority: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    console.log(`form submitted:`);
+    console.log(`Video Description: ${this.state.video_description}`);
+    console.log(`Video Responsible: ${this.state.video_responsible}`);
+    console.log(`Video Priority: ${this.state.video_priority}`);
+    console.log(`Video Completed: ${this.state.video_completed}`);
+
+    this.setState({
+      video_description: "",
+      video_responsible: "",
+      video_priority: "",
+      video_completed: false
+    });
+  };
+
   render() {
     return (
-      <div>
-        <h1>Create Video Page</h1>
+      <div style={{ marginTop: 20 }}>
+        <h3>Create New Video Lesson</h3>
+        <form onSubmit={this.onSubmit}>
+          <div className="form-group">
+            <label>Description:</label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.video_description}
+              onChange={this.onChangeVideoDescription}
+            />
+          </div>
+          <div className="form-group">
+            <label>Link:</label>
+            <input
+              type="text"
+              className="form-control"
+              value={this.state.video_responsible}
+              onChange={this.onChangeVideoResponsible}
+            />
+          </div>
+          <div className="form-group">
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="priorityOptions"
+                id="priorityLow"
+                value="Low"
+                checked={this.state.video_priority === "Low"}
+                onChange={this.onChangeVideoPriority}
+              />
+              <label className="form-check-label">Low</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="priorityOptions"
+                id="priorityMedium"
+                value="Medium"
+                checked={this.state.video_priority === "Medium"}
+                onChange={this.onChangeVideoPriority}
+              />
+              <label className="form-check-label">Medium</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input
+                className="form-check-input"
+                type="radio"
+                name="priorityOptions"
+                id="priorityHigh"
+                value="High"
+                checked={this.state.video_priority === "High"}
+                onChange={this.onChangeVideoPriority}
+              />
+              <label className="form-check-label">High</label>
+            </div>
+          </div>
+          <div className="form-group">
+            <input type="submit" value="Submit" className="btn btn-primary" />
+          </div>
+        </form>
       </div>
     );
   }
